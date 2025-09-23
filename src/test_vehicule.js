5092e4d1a0ef78be8f9695e9f6e120b662f27beb
KathyMunoz <katherine.munoz.moran@gmail.com>
2025-09-23 14:33:38 +0200

    ex_03

---
+++ src/test_vehicule.js
@@ -0,0 +1,1 @@
+const vehicule = new Vehicule("Mercedes CLK", 4, 250);
---
+++ src/vehicule.js
@@ -0,0 +1,7 @@
+export class Vehicule {
+    constructor(monVehiculeP, nbrRoueP, vitesseP) {
+        this.monVehicule = monVehicule;
+        this.nbrRoue = nbrRoue;
+        this.vitesse = vitesse;
+    }
+}
