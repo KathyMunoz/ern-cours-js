fabee5ddc06a0f4574ff8ae0164e903da30210c2
KathyMunoz <katherine.munoz.moran@gmail.com>
2025-09-23 16:06:40 +0200

    edited

---
+++ src/text_compteBancaire.js
@@ -0,0 +1,12 @@
+// Partie JS :
+// écouteur click sur le bouton id_create (qui va ajouter un compte au tableau de comptes),
+// écouteur click sur le bouton id_crediter (qui va créditer le compte du montant saisie dans  id_montant du compte sélectionné dans id_compte),
+// ecouteur click sur le bouton id_retirer (qui va retirer au compte id_compte le montant dans id_montant),
+
+
+const create = document.queryElementById("bt_create");
+
+addEventListener("click", function(bt_create) {
+    comptes.push
+
+})
