85bb24a6929beb5480f9eba1d559ec8bfcc16c6a
KathyMunoz <katherine.munoz.moran@gmail.com>
2025-09-23 17:00:57 +0200

    edited exo 7

--- src/text_compteBancaire.js
+++ src/text_compteBancaire.js
@@ -3,10 +3,14 @@
 // écouteur click sur le bouton id_crediter (qui va créditer le compte du montant saisie dans  id_montant du compte sélectionné dans id_compte),
 // ecouteur click sur le bouton id_retirer (qui va retirer au compte id_compte le montant dans id_montant),
 
+export default 
 
-const create = document.queryElementById("bt_create");
+const create = document.getElementById("bt_create");
+const champNom = document.getElementById("nom");
 
-addEventListener("click", function(bt_create) {
-    comptes.push
+create.addEventListener("click", function(event) {
+    const nomSaisi = champNom.value
+    const nouveauCompte = new CompteBancaire("nom") // value,pour recuperer la valeur tapée dans un input
+    comptes.push(nouveauCompte)
 
 })
