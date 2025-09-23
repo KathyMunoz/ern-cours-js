845877b6e52efd5d34eb0f4e4b6b48a536214b86
KathyMunoz <katherine.munoz.moran@gmail.com>
2025-09-23 22:18:01 +0200

    exo fini mais avec des questions

--- src/test_vehicule.js
+++ src/test_vehicule.js
@@ -1,1 +1,16 @@
-const vehicule = new Vehicule("Mercedes CLK", 4, 250);
+import Vehicule from "./vehicule.js";
+//instances de deux nouveaux vehicules
+const voiture = new Vehicule("Mercedes CLK", 4, 250);//objet: voiture
+const moto = new Vehicule("Honda CBR", 2, 280);//objet: moto
+
+
+//Affichage type de vehicule
+console.log("Le vehicule est de type : " + voiture.detect());
+console.log("Le vehicule est de type : " + moto.detect());
+
+//Aplication methode boost
+voiture.boost();
+console.log("La nouvelle vitesse est de : " + voiture.vitesse + "km/h");
+
+console.log(voiture.plusRapide());
+console.log(moto.plusRapide(voiture));
--- src/vehicule.js
+++ src/vehicule.js
@@ -1,7 +1,29 @@
-export class Vehicule {
-    constructor(monVehiculeP, nbrRoueP, vitesseP) {
-        this.monVehicule = monVehicule;
+export default class Vehicule  {
+    constructor(nomVehicule, nbrRoue, vitesse) {
+        this.nomVehicule = nomVehicule;
         this.nbrRoue = nbrRoue;
         this.vitesse = vitesse;
+    }
+     detect() {
+        if (this.nbrRoue == 4) {
+        return "voiture";
+        }
+        if (this.nbrRoue == 2) {
+            return "moto";
+        }   
+        return "Autre véhicule";   
+    }
+    boost () {
+        this.vitesse += 50;
+    }
+    plusRapide (vehicule) {//pour quoi v minuscule?
+        if (this.vitesse > vehicule.vitesse) {//vehicule.vitesse, vitesse est la propieté de vehicule ??
+            return `Le vehicule : ${this.nomVehicule} est le plus rapide`;
+        }
+        if (this.vitesse < vehicule.vitesse) {
+            return `Le vehicule : ${this.nomVehicule} est le plus rapide`
+        }
+        return "Les 2 vehicules vont à la meme vitesse";
+
     }
 }
