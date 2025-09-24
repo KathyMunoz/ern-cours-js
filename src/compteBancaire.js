66afa99028360e5c18629a75332b35aef1c80799
KathyMunoz <katherine.munoz.moran@gmail.com>
2025-09-24 11:57:04 +0200

    correction

--- src/compteBancaire.js
+++ src/compteBancaire.js
@@ -1,41 +1,69 @@
-import CompteBancaire from "./compteBancaire.js";
+export default class CompteBancaire {
 
-//Créer les comptes (tableau CompteBancaire)
-const comptes = [
-    new CompteBancaire("Alex"),
-    new CompteBancaire("Clovis"),
-    new CompteBancaire("Marco")
-];
+    /**
+     * Constructeur de la classe CompteBancaire
+     * @param {string} nom 
+     * @param {float} solde default 0 
+     */
+    constructor(nom, solde = 0) {
+        this.nom = nom;
+        this.solde = solde;
+    }
 
-//Assigner 1000 € à chaque compte
-for (const compte of comptes) {//compte pour appeler chaque position de mon tableau, je peux l'apeller aussi position par ex 
-    compte.crediter(1000);
-    console.log(`Ajout de : 1000 à : ${compte.nom}`);
-}
+    //Méthodes de la classe
 
-//Retirer 100 € à Alex
-try {
-    comptes[0].retirer(100);
-} catch (error) {
-    console.log(error.message);
-}
+    /**
+     * Méthode pour créditer d'un montant le compte
+     * @param {float} montant
+     * @return void
+     * @throws Error  
+     */
+    credit(montant) {
+        if (montant < 0) {
+            throw new Error(`Le montant ${montant} est négatif`);
+        }
+        if (isNaN(montant)) {
+            throw new Error(`Le montant : ${montant} n'est pas un nombre`);
+        }
+        this.solde += montant;
+    }
 
-//Virement de Marco 300 € à Clovis
-try {
-    comptes[2].virer(300, comptes[1]);
-} catch(error ) {
-    console.log(error.message);
-}
+    /**
+     * Méthode pour retirer un montant du compte
+     * @param {float} montant
+     * @return void
+     * @throws Error
+     */
+    retrait(montant) {
+        if (this.solde < montant) {
+            throw new Error(`Retrait impossible : Le solde du compte : ${this.nom} est inférieur à : ${montant}`);
+        }
+        if (montant < 0) {
+            throw new Error(`Le montant ${montant} est négatif`);
+        }
+        if (isNaN(montant)) {
+            throw new Error(`Le montant : ${montant} n'est pas un nombre`);
+        }
+        this.solde -= montant;
+    }
 
-//retrait de 1200 € d'Alex
+    /**
+     * Méthode pour virer de l'argent du compte courant (this) à un autre compte (compte)
+     * @param {float} montant 
+     * @param {CompteBancaire} compte
+     * @return void
+     * @throws Error  
+     */
+    virement(montant, compte) {
+        this.retrait(montant);
+        compte.credit(montant);
+    }
 
-try {
-    comptes[0].retirer(1200);
-} catch(error) {
-    console.log(error.message);
-}
-
-//Afficher le soldes des comptes
-for (const compte of comptes) {
-    compte.afficherCompte();
+    /**
+     * Méthode pour afficher le solde du compte avec ces informations
+     * @return string 
+     */
+    afficherCompte() {
+        return `Titulaire : ${this.nom}, solde : ${this.solde} €`;
+    }
 }
